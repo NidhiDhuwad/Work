9c2cf79b14695bfd937e3ba00b81caf17a141088
NidhiDhuwad <110440638+NidhiDhuwad@users.noreply.github.com>
2023-01-18 08:56:21 +0530

    Add files via upload

---
+++ reactproject/src/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HeaderFile from './CommonCompo/Headerfile';
+
+const Contact = () => {
+    return (
+        <div>
+            <HeaderFile/>
+            Contact Us Page
+        </div>
+    );
+};
+
+export default Contact;
